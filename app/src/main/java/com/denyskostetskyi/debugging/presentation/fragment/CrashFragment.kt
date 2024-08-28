685fbe60e058b81b8c65ac313a07145ad7bd6bdd
Denys Kostetskyi <denys.kostetskyi@globallogic.com>
2024-08-28 19:18:37 +0300

    Added CrashFragment

--- app/src/main/java/com/denyskostetskyi/debugging/presentation/fragment/CrashFragment.kt
+++ app/src/main/java/com/denyskostetskyi/debugging/presentation/fragment/CrashFragment.kt
@@ -6,10 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.denyskostetskyi.debugging.databinding.FragmentNotificationsBinding
+import com.denyskostetskyi.debugging.R
+import com.denyskostetskyi.debugging.databinding.FragmentCrashBinding
+import kotlin.random.Random
 
-class NotificationsFragment : Fragment() {
-    private var _binding: FragmentNotificationsBinding? = null
+class CrashFragment : Fragment() {
+    private var _binding: FragmentCrashBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +24,7 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentCrashBinding.inflate(inflater, container, false)
         Log.v(TAG, "onCreateView")
         return binding.root
     }
@@ -30,6 +32,39 @@ class NotificationsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Log.v(TAG, "onViewCreated")
+        setButtonClickListener()
+    }
+
+    private fun setButtonClickListener() {
+        binding.buttonCauseRandomCrash.setOnClickListener {
+            causeRandomCrash()
+        }
+    }
+
+    private fun causeRandomCrash() {
+        when (Random.nextInt(3)) {
+            0 -> throwNullPointerException()
+            1 -> modifyUiOnBackgroundThread()
+            2 -> causeApplicationNotResponding()
+        }
+    }
+
+    private fun throwNullPointerException() {
+        val string: String? = null
+        Log.d(TAG, "${string!!.length}")
+    }
+
+    private fun modifyUiOnBackgroundThread() {
+        Thread {
+            binding.buttonCauseRandomCrash.text = getString(R.string.crash)
+        }.start()
+    }
+
+    private fun causeApplicationNotResponding() {
+        while (true) {
+            Thread.sleep(SLEEP_DURATION)
+            Log.d(TAG, "Blocking main thread")
+        }
     }
 
     override fun onStart() {
@@ -69,6 +104,7 @@ class NotificationsFragment : Fragment() {
     }
 
     companion object {
-        private const val TAG = "NotificationsFragment"
+        private const val TAG = "CrashFragment"
+        private const val SLEEP_DURATION = 1000L
     }
 }
