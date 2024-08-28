364a3f0d59677c22c089e7aaa5963e446ee51bdb
Denys Kostetskyi <denys.kostetskyi@globallogic.com>
2024-08-28 18:16:06 +0300

    Added memory leaks in MemoryLeakFragment

--- app/src/main/java/com/denyskostetskyi/debugging/presentation/fragment/MemoryLeakFragment.kt
+++ app/src/main/java/com/denyskostetskyi/debugging/presentation/fragment/MemoryLeakFragment.kt
@@ -1,16 +1,31 @@
 package com.denyskostetskyi.debugging.presentation.fragment
 
+import android.content.Context
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.denyskostetskyi.debugging.data.Database
 import com.denyskostetskyi.debugging.databinding.FragmentMemoryLeakBinding
+
 
 class MemoryLeakFragment : Fragment() {
     private var _binding: FragmentMemoryLeakBinding? = null
     private val binding get() = _binding!!
+
+    private val handler = Handler(Looper.getMainLooper())
+    private lateinit var database: Database
+    private var isBackgroundTaskRunning = false
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        database = Database.getInstance(context)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +38,31 @@ class MemoryLeakFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentMemoryLeakBinding.inflate(inflater, container, false)
+        initViews()
         Log.w(TAG, "onCreateView")
         return binding.root
+    }
+
+    private fun initViews() {
+        binding.buttonGetValueFromDatabase.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val value = database.value
+                Toast.makeText(requireContext(), "Value: $value", Toast.LENGTH_SHORT).show()
+            }
+        })
+        binding.buttonStartBackgroundTask.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                startBackgroundTask()
+            }
+        })
+    }
+
+    private fun startBackgroundTask() {
+        if (!isBackgroundTaskRunning) {
+            isBackgroundTaskRunning = handler.postDelayed({
+                binding.textViewDelayedTaskResult.text = "Delayed task finished"
+            }, TASK_POST_DELAY)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -55,7 +93,6 @@ class MemoryLeakFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         Log.w(TAG, "onDestroyView")
-        _binding = null
     }
 
     override fun onDestroy() {
@@ -70,5 +107,6 @@ class MemoryLeakFragment : Fragment() {
 
     companion object {
         private const val TAG = "MemoryLeakFragment"
+        private const val TASK_POST_DELAY = 20000L
     }
 }
